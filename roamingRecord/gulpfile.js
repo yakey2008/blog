43d1fc0924719acabba196e92387e34b96f09777
slimeball <slimeball@foxmail.com>
2017-05-31 17:22:15 +0800

    添加压缩js,css,开发版,生产版html入口

--- roamingRecord/gulpfile.js
+++ roamingRecord/gulpfile.js
@@ -1,24 +1,56 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const minifycss = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const del = require('del');
 const reload = browserSync.reload;
 
-gulp.task('serve', ['sass'], function() {
-
+//del dist directory
+gulp.task('del', function (cb) {
+    return del(['dist'], cb);
+})
+//run serve compile code ,watch code change
+gulp.task('serve', ['sass'], () => {
     browserSync.init({
         server: "./"
     });
-
-    gulp.watch("css/*.scss", ['sass']);
+    gulp.watch("scss/*.scss", ['sass']);
     gulp.watch("*.html").on('change', reload);
+    gulp.watch("js/*.js").on('change', reload);
 });
+//dist code
+gulp.task('html',()=>{
+    return gulp.src('./index.production.html')
+    .pipe(rename('./index.html'))
+})
+gulp.task('style', () => {
+    return gulp.src(['./lib/css/*.css','./css/*.css'])
+        .pipe(concat('main.css'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/css/'))
+})
+
+gulp.task('js',()=>{
+    return gulp.src(['./lib/js/jquery.min.js','./lib/js/*.js','./js/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'))
+})
 
 // scss编译后的css将注入到浏览器里实现更新
-gulp.task('sass', function() {
-    return gulp.src("css/*.scss")
+gulp.task('sass', () => {
+    return gulp.src("scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("dist/css"))
-        .pipe(reload({stream: true}));
+        .pipe(gulp.dest("css"))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
+gulp.task('build',['style','js']);
 gulp.task('default', ['serve']);
--- roamingRecord/js/main.js
+++ roamingRecord/js/main.js
@@ -1,11 +1,50 @@
+/**
+ * 自定义js，插件初始化，插件操作
+ * 邹贵初
+ * T00005162@vipshop.com
+ */
+
 jQuery(function ($) {
-    $('#js-select2').select2({
+    //mask摸态框显示隐藏
+    var hideMask = function (method) {
+        $pageMask.addClass('ds-n');
+        $customSaerch.addClass('ds-n');
+    }
+
+    var showMask = function () {
+        $pageMask.removeClass('ds-n');
+        $customSaerch.removeClass('ds-n');
+    }
+    //初始化select2
+    var $select2 = $('#js-select2'),
+        $pageMask = $('#js-page-mask'),
+        $customSaerch = $('#js-custom-saerch');
+
+
+    $select2.select2({
         minimumResultsForSearch: Infinity,
         width: '180px'
     })
 
+    $select2.on("select2:closing", function (e) {
+        if (e.currentTarget.value === 'costom') {
+            showMask();
+        }
+    });
+    
+    $select2.on("select2:select", function (e) {
+        if (e.currentTarget.value === 'costom') {
+            showMask();
+            (function (dom) {
+                dom.on('click', function () {
+                    showMask();
+                })
+            })($('#select2-js-select2-results').find('li:last'));
+        }
+    });
+
     //wrapper高度减上下外边
-    $('.msg-item-container').height($('.wrapper').height() - 100);
+    $('.msg-item-container').height($('.wrapper').height() - 120);
     //wrapper高度减30上边30下边
     $('.aside').height($('.wrapper').height() - 30 - 30);
     //左侧滚动条优化
@@ -21,4 +60,85 @@ jQuery(function ($) {
     //兼容ie placeholder
     $('.search-input').placeholder();
 
+
+    //时间配置初始化
+    $('[data-toggle="datepicker"]').datepicker({
+        autoHide: true,
+        format: 'yyyy年mm月dd日',
+        weekStart: 1,
+        yearSuffix: '年',
+        months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        monthsShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        days: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+        daysShort: ['一', '二', '三', '四', '五', '六', '日'],
+        daysMin: ['日', '一', '二', '三', '四', '五', '六'],
+    });
+
+
+    //时间选择处理 摸态框关闭 提交按钮事件
+    var pickTime = function () {
+        var jsStart = $('#js-start-time'),
+            jsEnd = $('#js-end-time'),
+            pickedTime = '';
+        jsStart.on('pick.datepicker', function (e) {
+            if (!pickedTime) {
+                pickedTime = e.date;
+            } else {
+                if (e.date > pickedTime) {
+                    swal('结束时间不可小于开始时间');
+                    e.preventDefault();
+                } else {
+                    $(this).val(e.date)
+                }
+            }
+
+        });
+        jsEnd.on('pick.datepicker', function (e) {
+            if (!pickedTime) {
+                pickedTime = e.date;
+            } else {
+                if (e.date < pickedTime) {
+                    swal('结束时间不可小于开始时间');
+                    e.preventDefault();
+                } else {
+                    $(this).val(e.date)
+                }
+            }
+
+        });
+        $('#js-close-btn').on('click', function () {
+            hideMask();
+        })
+        $('#js-picktimebtn').on('click', function () {
+            hideMask();
+        })
+    }
+
+    //搜索事件
+    var sendajax = function (data) {
+        alert('发送查询');
+        //$.ajax({data})
+    }
+    var searchEvt = function (dom, evt) {
+        var data = '';
+        if (evt === 'keydown') {
+            dom.on(evt, function (e) {
+                data = $('#js-select2').val();
+                if (e.keyCode === 13) {
+                    sendajax(data);
+                }
+            })
+        } else {
+            dom.on(evt, function () {
+                data = $('#js-select2').val();
+                sendajax(data);
+            })
+        }
+    }
+
+    //执行时间选择处理函数
+    pickTime();
+    //搜索事件
+    searchEvt($('#js-searchbtn'), 'click');
+    searchEvt($('#js-searchinput'), 'keydown');
 })
